9e59135f13728350e10e45fe4b948842c63977e2
Tovfikur Rahman <tovfikur@gmail.com>
2024-06-05 21:40:17 +0600

    .

--- apis/start_frontend.sh
+++ apis/start_frontend.sh
@@ -1,37 +1,50 @@
-# install npm
-# install required packages
+# # install npm
+# # install required packages
 
-APP_NAME="frontend"
-PROJECT_NAME="Khukumoni"
-VIEWS_FILE="../$PROJECT_NAME/$APP_NAME/views.py"
-URLS_FILE="../$PROJECT_NAME/$APP_NAME/urls.py"
-SETTINGS_FILE="../$PROJECT_NAME/$PROJECT_NAME/settings.py"
-PROJECT_URLS_FILE="../$PROJECT_NAME/$PROJECT_NAME/urls.py"
-
-
-
-# Step 3: Create views.py
-cat <<EOF > $VIEWS_FILE
+# APP_NAME="frontend"
+# PROJECT_NAME="Khukumoni"
+# VIEWS_FILE="../$PROJECT_NAME/$APP_NAME/views.py"
+# URLS_FILE="../$PROJECT_NAME/$APP_NAME/urls.py"
+# SETTINGS_FILE="../$PROJECT_NAME/$PROJECT_NAME/settings.py"
+# PROJECT_URLS_FILE="../$PROJECT_NAME/$PROJECT_NAME/urls.py"
+# APP_URL = "../$PROJECT_NAME/$APP_NAME/"
 
 
 
-EOF
+# # Step 3: Create views.py
+# cat <<EOF > $VIEWS_FILE
+
+
+
+# EOF
 
 
 
 
-# Step 4: Create urls.py
-cat <<EOF > $URLS_FILE
+# # Step 4: Create urls.py
+# cat <<EOF > $URLS_FILE
 
 
 
-EOF
+# EOF
 
 
-# Step 6: Add the app path to the main project urls.py if not already included
-if ! grep -q "path('', include('$APP_NAME.urls'))" "$PROJECT_URLS_FILE"; then
-    sed -i "/urlpatterns = \[/a \ \ \ \ path('', include('$APP_NAME.urls'))," $PROJECT_URLS_FILE
-fi
+# mkdir -p $APP_URL/templates/
+# mkdir -p $APP_URL/static/
+# mkdir -p $APP_URL/static/css/
+# mkdir -p $APP_URL/static/frontend/
+# mkdir -p $APP_URL/static/images/
+# mkdir -p $APP_URL/src
+# mkdir -p $APP_URL/src/components
 
-# Display success message
-echo "API setup for $APP_NAME completed."
+
+# cd ../$PROJECT_NAME/$APP_NAME/
+# npm init -y
+
+# # Step 6: Add the app path to the main project urls.py if not already included
+# if ! grep -q "path('', include('$APP_NAME.urls'))" "$PROJECT_URLS_FILE"; then
+#     sed -i "/urlpatterns = \[/a \ \ \ \ path('', include('$APP_NAME.urls'))," $PROJECT_URLS_FILE
+# fi
+
+# # Display success message
+# echo "API setup for $APP_NAME completed."
