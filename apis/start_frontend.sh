57f4e7b80cff93d4ef8d6f32c0e3b41fca6392c6
Tovfikur Rahman <tovfikur@gmail.com>
2024-06-06 18:16:11 +0600

    .

--- apis/start_frontend.sh
+++ apis/start_frontend.sh
@@ -1,4 +1,6 @@
-# install react "npx create-react-app reactive"
+# install react "npx create-next-app reactive"
 # create react app
 # install packages
 # npm install bootstrap reactstrap --legacy-peer-deps
+
+touch test-package-of 
